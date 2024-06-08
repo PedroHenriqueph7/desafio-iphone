1db318b99cafd3d2d29756dce85cf21104edbcb9
PedroHenriqueph7 <pedrohenrizzx7@gmail.com>
2024-06-07 21:51:19 -0300

    projeto java

--- src/funcionalidadeDoIphone/IphoneTeste.java
+++ src/funcionalidadeDoIphone/IphoneTeste.java
@@ -1,25 +1,38 @@
 package funcionalidadeDoIphone;
 
+import java.util.Scanner;
+
 import iphone.Iphone;
 
 public class IphoneTeste {
-    public static void main(String[] args) {
-        Iphone iphone = new Iphone();
 
-        System.out.println("Funções do Iphone"); 
-    
-        System.out.println(iphone.ligar("Ligando para: 85-8473828"));
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        Iphone iphone = new Iphone();
+        String numero;
+        String musica;
+        String url;
+
+        System.out.println("Informe o número para a ligação: "); 
+        numero = scanner.nextLine();
+        iphone.ligar(numero); 
         iphone.atender();
         iphone.iniciarCorreioVoz();
 
         iphone.tocar();
         iphone.pausar();
-        System.out.println(iphone.selecionarMusica("Selecionando musica: sol"));
+        System.out.println("Selecione a musica: ");
+        musica = scanner.nextLine();
+        iphone.selecionarMusica(musica);
 
-        System.out.println(iphone.exibirPagina("exibindo pagina: pagina.url.com"));
+        System.out.println("Informe o url da página: ");
+        url = scanner.nextLine();
+        iphone.exibirPagina(url);
         iphone.adicionarNovaAba();
         iphone.atualizarPagina();
 
-        
+     scanner.close();   
     }
 }
